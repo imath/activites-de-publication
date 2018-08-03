8f5a7b5a15e7764b91259e0f20e444ee4b099e5c
imath <math.viet@gmail.com>
2018-08-03 13:24:28 +0200

    Add missing templates dir from previous commit

---
+++ templates/buddypress/common/js-templates/activity/entry.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Publication Activity entry JS Template
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+?>
+<script type="text/html" id="tmpl-activites-de-publication">
+    <article class="comment-body">
+        <footer class="comment-meta">
+            <div class="comment-author vcard">
+                <img alt="" src="{{data.user_avatar.full}}" class="avatar avatar-100 photo" height="100" width="100">
+                <b class="fn"><a href="{{data.user_link}}" rel="nofollow" class="url">{{data.user_name}}</a></b>
+            </div>
+            <div class="comment-metadata">
+                <a href="{{data.link}}">
+                    <time datetime="{{data.date}}">{{{data.human_date}}}</time>
+                </a>
+                <# if ( data.edit_link ) { #>
+                    <span class="edit-link">
+                        <a class="comment-edit-link" href="{{data.edit_link}}"><?php esc_html_e( 'Modifier', 'activites-d-article' ); ?></a>
+                    </span>
+                <# } #>
+            </div>
+        </footer>
+        <div class="comment-content">
+            {{{data.content}}}
+        </div>
+    </article>
+</script>
