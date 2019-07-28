d6e83b66160918ddb29cb308e5dd374cc1ec0572
imath <math.viet@gmail.com>
2019-07-28 09:33:25 +0200

    Make sure the current discussion type views are shown even if the site admin edits the template

--- templates/buddypress/common/js-templates/activity/activites-de-publication.php
+++ templates/buddypress/common/js-templates/activity/activites-de-publication.php
@@ -5,7 +5,7 @@
  * @package Activites_de_Publication\templates\buddypress\common\js-templates\activity
  *
  * @since 1.0.0
- * @version 1.0.0
+ * @version 2.0.0
  */
 ?>
 <script type="text/html" id="tmpl-activites-de-publication-nav">
